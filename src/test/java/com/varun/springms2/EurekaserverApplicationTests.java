6f2e2d098a8b9556a6b85980f6485254bf740389
varun <varun9100@gmail.com>
2020-05-31 21:57:02 -0500

    First Commit

---
+++ src/test/java/com/varun/springms2/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.springms2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
